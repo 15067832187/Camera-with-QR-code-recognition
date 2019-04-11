db7cf8b059bb50beec454b256d2b03e375a37d98
15067832187 <dltszlym@outlook.com>
2019-04-11 17:01:50 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace MyCa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.vp = new AForge.Controls.VideoSourcePlayer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // vp
+            // 
+            this.vp.Location = new System.Drawing.Point(40, 80);
+            this.vp.Margin = new System.Windows.Forms.Padding(4);
+            this.vp.Name = "vp";
+            this.vp.Size = new System.Drawing.Size(460, 385);
+            this.vp.TabIndex = 1;
+            this.vp.Text = "videPlayer";
+            this.vp.VideoSource = null;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(36, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "请选择需要连接的摄像设备：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(298, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 26);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(526, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(40, 495);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(594, 94);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(526, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "断开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(526, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 32);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "拍照";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(526, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 32);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "开始解析";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Location = new System.Drawing.Point(526, 368);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 32);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "结束解析";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 638);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer vp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,234 @@
+﻿#region 初始引用
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+#endregion
+
+#region 额外引用
+using System.IO;
+using ZXing;
+using AForge.Video.DirectShow;
+using Size = System.Drawing.Size;
+#endregion
+
+namespace MyCa
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region  事件集合
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Waitlink();//进入等待连接状态
+            Lb1Change("程序启动成功，请选择视频输入设备并连接");
+            LoadCa();
+        } 
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {CloseLink();}
+ 
+        private void Button1_Click(object sender, EventArgs e)//连接开始按钮
+        {
+            Lb1Change("连接建立中...");
+            CameraLink();
+            Linking();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)//断开连接按钮
+        {
+            Waitlink();
+            CloseLink();
+        }      
+
+        private void Button3_Click(object sender, EventArgs e)//拍照按钮
+        {
+            Takephoto();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)//解析开始按钮
+        {
+            Jiexing();
+        }
+
+        private void Button5_Click(object sender, EventArgs e)//结束解析按钮
+        {
+            Linking();
+        }
+        #endregion
+
+        #region  方法集合
+        private void LoadCa()//加载视频输入设备
+        {
+            try
+            {
+                //枚举所有视频输入设备
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+                foreach (FilterInfo device in videoDevices)
+                {
+                    comboBox1.Items.Add(device.Name);
+                    Lb1Change("检测到视频输入设备，请选择");
+                }
+                comboBox1.SelectedIndex = 0;
+            }
+            catch (ApplicationException)
+            {
+                comboBox1.Items.Add("未检测到视频输入设备");
+                Lb1Change("未检测到视频输入设备，请检测您的设备完好，谢谢");
+            }
+        }
+
+        private void CameraLink()//连接视频输入设备
+        {
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[comboBox1.SelectedIndex].MonikerString);
+            videoSource.DesiredFrameSize = new Size(320, 240);
+            videoSource.DesiredFrameRate = 1;
+            vp.VideoSource = videoSource;
+            vp.Start();
+            Lb1Change("连接建立成功！");
+        }
+
+        private void CloseLink()//关闭连接
+        {
+            Lb1Change("检测到用户断开连接，开始断开");
+            vp.SignalToStop();
+            vp.WaitForStop();
+            Lb1Change("断开成功！");
+        }
+        
+        private void Lb1Change(string instring)//下方提示
+        {
+            listBox1.Items.Add(DateTime.Now.ToLongTimeString().ToString()+"    "+instring);
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+
+        public void Takephoto()//拍照功能
+        {
+            try
+            {
+                Bitmap bitmap = new Bitmap(460, 385);
+                Rectangle rect1 = new Rectangle(40, 80, 460, 385);
+                vp.DrawToBitmap(bitmap, rect1);
+                string fileName = "D:/temp/未命名图片.jpg";
+                bitmap.Save(fileName, ImageFormat.Jpeg);
+                Lb1Change(fileName + "    保存成功!");
+                bitmap.Dispose();
+            }
+            catch (Exception)
+            {
+                Lb1Change("拍照失败");
+            }
+        }
+
+        public void Alluers()//解析功能整合
+        {
+            try
+            {
+                Bitmap bitmap = new Bitmap(460, 385);
+                Rectangle rect1 = new Rectangle(40, 80, 460, 385);
+                vp.DrawToBitmap(bitmap, rect1);
+                BarcodeReader reader = new BarcodeReader();
+                Result result = reader.Decode(bitmap);
+                string resultText = result.Text;
+                Lb1Change("解析成功，正在转入：" + resultText);
+                Lb1Change("解析结束");
+                Linking();
+                System.Diagnostics.Process.Start("iexplore.exe", resultText);//打开浏览器
+            }
+            catch (Exception)
+            {
+                 Lb1Change("解析失败,请将二维码对准摄像头");
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)//1000ms重新解析一次
+        {
+            Alluers();
+        }
+        #endregion
+
+        #region 快捷状态切换方法
+        public void Waitlink()//等待连接：（开启：连接）（禁用：断开连接，拍照，解析相关）
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            timer1.Enabled = false;
+        }
+
+        public void Linking()//连接中（开启：断开连接，拍照，开始解析）（禁用：连接，结束解析）
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = false;
+            timer1.Enabled = false;
+        }
+
+        public void Jiexing()//解析中（开启：结束解析）（禁用：连接相关，拍照，开始解析）
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+            timer1.Enabled = true;
+        }
+        #endregion
+
+        #region  备用：删除保存的图片
+        /// <summary>
+        /// 删除文件夹以及文件
+        /// </summary>
+        /// <param name="directoryPath"> 文件夹路径 </param>
+        /// <param name="fileName"> 文件名称 </param>
+        public static void DeleteDirectory(string directoryPath, string fileName)
+        {
+
+            //删除文件
+            for (int i = 0; i < Directory.GetFiles(directoryPath).ToList().Count; i++)
+            {
+                if (Directory.GetFiles(directoryPath)[i] == fileName)
+                {
+                    File.Delete(fileName);
+                }
+            }
+        }
+        #endregion
+
+        #region  备用：解析二维码初始代码
+        private void Startjx()//解析二维码
+        {
+            try
+            {
+                string path = "D:/temp/未命名图片.jpg";
+                FileStream fs = new FileStream(path, FileMode.Open);
+                Bitmap bitmap = Bitmap.FromStream(fs) as Bitmap;
+                BarcodeReader reader = new BarcodeReader();
+                Result result = reader.Decode(bitmap);
+                string resultText = result.Text;
+                Lb1Change("解析成功，正在转入："+resultText);
+                Lb1Change("解析结束");
+                timer1.Enabled = false;
+                System.Diagnostics.Process.Start("iexplore.exe", resultText);
+            }
+            catch (Exception)
+            {
+                Lb1Change("解析失败,请将二维码对准摄像头");
+                //DeleteDirectory("D:/temp", "未命名图片.jpg");
+            }
+        }
+        #endregion
+    }
+}
